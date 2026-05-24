d8b5922bf994c03f449dc371fca9be6631f0a458
arjun puri <arjunpuri991@gmail.com>
2026-05-25 00:22:27 +0530

    new

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import AppFrame from './components/AppFrame'
+import Script from "next/script"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -78,6 +79,33 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body className={inter.className}>
+        <Script id="yandex-metrica" strategy="afterInteractive">
+          {`
+(function(m,e,t,r,i,k,a){
+    m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();
+    for (var j = 0; j < document.scripts.length; j++) {
+      if (document.scripts[j].src === r) { return; }
+    }
+    k=e.createElement(t),
+    a=e.getElementsByTagName(t)[0],
+    k.async=1,
+    k.src=r,
+    a.parentNode.insertBefore(k,a)
+})(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=109395170', 'ym');
+
+ym(109395170, 'init', {
+    ssr:true,
+    webvisor:true,
+    clickmap:true,
+    ecommerce:"dataLayer",
+    referrer: document.referrer,
+    url: location.href,
+    accurateTrackBounce:true,
+    trackLinks:true
+});
+`}
+        </Script>
         <AppFrame>
           {children}
         </AppFrame>
